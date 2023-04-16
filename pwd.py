f458116db4bdf4e9ceced2da5035c55ad6289a62
shiulegend <shiulegend@gmail.com>
2023-04-16 14:24:06 +0800

    change pwd

---
+++ pwd.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  7 20:36:25 2023
+
+@author: shiul
+"""
+#for i in range(1, 10):
+ #   for j in range(1, 10):
+  #      print("%d * %d = %d" % (i, j, i*j))
+   # print()
+   
+password = '123456789'
+i =  3 
+while i > 0 :
+    pwd = input('請輸入密碼')
+    if pwd == password:
+        print('登入成功!')
+        bradk
+    else :
+        i = i - 1 
+        print ('密碼錯誤~還有' , i , '次機會')
+   
